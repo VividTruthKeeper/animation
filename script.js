c6780bac9da2f1a4fc35e76c7ec5b8c3359249af
VividTruthKeeper <mr.aydogdy02@gmail.com>
2021-08-30 09:25:34 +0500

    removed comments

--- script.js
+++ script.js
@@ -1,21 +1,19 @@
 let animBlocks = document.querySelectorAll('.animation-block');
 
 
-// window.addEventListener('click', () => {
     let counter = 0;
     setInterval(() => {
         if(counter === animBlocks.length){
             clearInterval;
         }else{
-            bgAnimationEnder(animBlocks[counter]);
+            bgAnimator(animBlocks[counter]);
             counter++;
         }
 
     }, 400);
-// })
 
 
-function bgAnimationEnder(element){
+function bgAnimator(element){
     element.classList.add('active');
     setTimeout(() => {
         element.classList.add('fading')
